00e9d487783df6653c448d0d656a3272415293f5
Winkley1 <winkley_2@hotmail.com>
2023-06-05 13:20:44 -0300

    finalizado o projero.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner texto = new Scanner(System.in);
+        double Saldo = 237.48;
+        int Numero;
+        String Agencia = "067-8";
+        String NomeCliente = "MARIO ANDRADE";
+
+        System.out.println("Por favor, digite o número da Agência !");
+        Numero = texto.nextInt();
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo " + Saldo + " já está disponível para saque.");
+
+
     }
 }
